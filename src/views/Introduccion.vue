f550e4ea1edd297bac7b879c96cbf13bb227ad87
Andrea Botello <andrea.botellor@misena.edu.co>
2025-08-22 09:16:10 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -22,8 +22,8 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/hrH1-dOJCf4?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video. Fundamentos financieros para emprender 
 
 </template>
 
